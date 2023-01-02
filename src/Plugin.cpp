56221c593a20d3acc21bf3997d9203e70341dc82
Patrick Reed <62087359+patpatcodeattack@users.noreply.github.com>
2023-01-01 20:12:51 -0600

    remove all

--- src/Plugin.cpp
+++
@@ -1,8 +0,0 @@
-#include <RNBO_JuceAudioProcessor.h>
-
-//This creates new instances of your plugin, change RNBO::JuceAudioProcessor()
-//to your own class if you create a derived plugin
-AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-	return new RNBO::JuceAudioProcessor();
-}
